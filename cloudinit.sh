4e88218252f05710476d4036eb186e23f6a5ef23
gadube <gadube@node.pszrun.vectorizationsz-pg0.clemson.cloudlab.us>
2020-11-19 15:17:19 -0500

    correcting localegen

--- cloudinit.sh
+++ cloudinit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Fix locale warnings
-localedef -c -f UTF-8 -i en_US en_US.UTF-8
+sudo localedef -c -f UTF-8 -i en_US en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
 #Install newer version of gcc
