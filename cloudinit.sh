d5bd6965ac0267a3439fca3ddb2d4e1e8020d708
gadube <gadube@node.pszrun.vectorizationsz-pg0.clemson.cloudlab.us>
2020-11-20 10:32:25 -0500

    Updating init order

--- cloudinit.sh
+++ cloudinit.sh
@@ -4,12 +4,6 @@
 sudo localedef -c -f UTF-8 -i en_US en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-#Install newer version of gcc
-sudo yum install -y centos-release-scl
-sudo yum install -y devtoolset-9
-scl enable devtoolset-9 bash
-gcc --version
-
 #Install PAPI
 git clone https://bitbucket.com/icl/papi.git
 cd papi/src
@@ -18,6 +12,13 @@ make
 make install
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/papi/src
 sudo sh -c 'echo -1 >/proc/sys/kernel/perf_event_paranoid'
+
+#Install newer version of gcc
+sudo yum install -y centos-release-scl
+sudo yum install -y devtoolset-9
+scl enable devtoolset-9 bash
+gcc --version
+
 
 #Install CUDA
 #wget https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-10.0.130-1.x86_64.rpm
