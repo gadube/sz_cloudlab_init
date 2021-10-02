c111d1d2b1859fc0ebf6025185cbe3029358779e
gadube <gadube@node0.vecsz-gold.vectorizationsz-pg0.clemson.cloudlab.us>
2021-10-02 12:10:48 -0400

    Updating Repo name and necessary SW

--- cloudinit.sh
+++ cloudinit.sh
@@ -4,29 +4,10 @@
 sudo localedef -c -f UTF-8 -i en_US en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-#Install PAPI
-git clone https://bitbucket.com/icl/papi.git /users/gadube/papi
-cd /users/gadube/papi/src
-./configure
-make
-make install
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/users/gadube/papi/src
-sudo sh -c 'echo -1 >/proc/sys/kernel/perf_event_paranoid'
-
 #Install newer version of gcc
 sudo yum install -y centos-release-scl
 sudo yum install -y devtoolset-9
-#scl enable devtoolset-9 bash
-#gcc --version
-
-
-#Install CUDA
-#wget https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-10.0.130-1.x86_64.rpm
-#rpm -i cuda-repo-*.rpm
-#sudo yum install -y cuda
-#export PATH=/usr/local/cuda/bin:$PATH
-#export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
 cd /users/gadube 
-git clone https://github.com/gadube/cuSZ.git
-sudo setfacl -R -m user:gadube:rwx cuSZ 
+git clone https://github.com/szcompressor/vecSZ.git
+sudo setfacl -R -m user:gadube:rwx vecSZ 
